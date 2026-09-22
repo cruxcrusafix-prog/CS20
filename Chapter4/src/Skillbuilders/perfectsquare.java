546cd88ec8da45eeb781c78f1204bc38817b8351
510321002 <510321002@L815WD220016.edu.cbe.ab.ca>
2026-09-22 10:07:27 -0600

    10:07 AM

---
+++ Chapter4/src/Skillbuilders/perfectsquare.java
@@ -0,0 +1,45 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class perfectsquare {
+	public static void main(String[] args) {
+		Scanner userinput = new Scanner(System.in); {}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
